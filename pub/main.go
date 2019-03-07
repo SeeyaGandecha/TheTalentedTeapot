6a65a9e04c50a3f054c5b5c6a9978e1213360093
Seeya Gandecha <seeya@gandecha.net>
2019-03-07 12:17:58 +0000

    account combination added (workiing model from two git repo clones)

--- pub/main.go
+++ pub/main.go
@@ -12,7 +12,7 @@ var tpl *template.Template
 
 type PageData struct {
 	Title     string
-	FirstName string
+	//FirstName string
 }
 
 func init() {
@@ -28,7 +28,7 @@ func main() {
 	http.HandleFunc("/forum", frm)
 	http.HandleFunc("/maps", maps)
 	http.HandleFunc("/contact", cntct)
-	http.HandleFunc("/account", acct)
+	//http.HandleFunc("/account", acct)
 	http.HandleFunc("/TTTabout", TTTabt)
 	http.HandleFunc("/TTTfeedback", TTTfdbk)
 	http.HandleFunc("/TTTevents", TTTevents)
@@ -41,11 +41,21 @@ func main() {
 
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
 
+
 	govalidator.SetFieldsRequiredByDefault(true)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir("files"))))
+	//http.HandleFunc("/account", account)
+	//http.HandleFunc("/signup", signup)
+	//http.HandleFunc("/signin", signin)
+	//http.HandleFunc("/internal", inaccount)
+	//http.HandleFunc("/logout", logout)
 
 	http.ListenAndServe(":8080", nil)
 
 }
+
+
 
 func idx(w http.ResponseWriter, req *http.Request) {
 	pd := PageData{
@@ -128,27 +138,27 @@ func cntct(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 }
-func acct(w http.ResponseWriter, req *http.Request) {
-
-	pd := PageData{
-		Title: "Account",
-	}
-
-	var firstname string
-
-	if req.Method == http.MethodPost {
-
-		firstname = req.FormValue("fname")
-		pd.FirstName = firstname
-	}
-
-	err := tpl.ExecuteTemplate(w, "account.gohtml", pd)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-}
+//func acct(w http.ResponseWriter, req *http.Request) {
+//
+//	pd := PageData{
+//		Title: "Account",
+//	}
+//
+//	var firstname string
+//
+//	if req.Method == http.MethodPost {
+//
+//		firstname = req.FormValue("fname")
+//		pd.FirstName = firstname
+//	}
+//
+//	err := tpl.ExecuteTemplate(w, "account.gohtml", pd)
+//	if err != nil {
+//		log.Println(err)
+//		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+//		return
+//	}
+//}
 
 func TTTabt(w http.ResponseWriter, req *http.Request) {
 
@@ -177,6 +187,7 @@ func TTTfdbk(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 }
+
 func TTTevents(w http.ResponseWriter, req *http.Request) {
 
 	pd := PageData{
@@ -245,31 +256,141 @@ func TTTsm(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 }
+//
+//func signup(w http.ResponseWriter, r *http.Request) {
+//	switch r.Method {
+//	case "GET":
+//		u := &User{}
+//		u.Errors = make(map[string]string)
+//		u.Errors["lname"] = getMsg(w, r, "lname")
+//		u.Errors["fname"] = getMsg(w, r, "fname")
+//		u.Errors["email"] = getMsg(w, r, "email")
+//		u.Errors["username"] = getMsg(w, r, "username")
+//		u.Errors["password"] = getMsg(w, r, "password")
+//		render(w, "signup", u)
+//	case "POST":
+//		if n := checkUser(r.FormValue("userName")); n == true {
+//			setMsg(w, "username", "User already exists. Please enter a unique username!")
+//			http.Redirect(w, r, "/signup", 302)
+//			return
+//		}
+//		u := &User{
+//			Uuid:     Uuid(),
+//			Fname:    r.FormValue("fName"),
+//			Lname:    r.FormValue("lName"),
+//			Email:    r.FormValue("email"),
+//			Username: r.FormValue("userName"),
+//			Password: r.FormValue("password"),
+//		}
+//		result, err := govalidator.ValidateStruct(u)
+//		if err != nil {
+//			e := err.Error()
+//			if re := strings.Contains(e, "Lname"); re == true {
+//				setMsg(w, "lname", "Please enter a valid Last Name")
+//			}
+//			if re := strings.Contains(e, "Email"); re == true {
+//				setMsg(w, "email", "Please enter a valid Email Address!")
+//			}
+//			if re := strings.Contains(e, "Fname"); re == true {
+//				setMsg(w, "fname", "Please enter a valid First Name")
+//			}
+//			if re := strings.Contains(e, "Username"); re == true {
+//				setMsg(w, "username", "Please enter a valid Username!")
+//			}
+//			if re := strings.Contains(e, "Password"); re == true {
+//				setMsg(w, "password", "Please enter a Password!")
+//			}
+//
+//		}
+//		if r.FormValue("password") != r.FormValue("cpassword") {
+//			setMsg(w, "password", "The passwords you entered do not Match!")
+//			http.Redirect(w, r, "/signup", 302)
+//			return
+//		}
+//
+//		if result == true {
+//			u.Password = enyptPass(u.Password)
+//			saveData(u)
+//			http.Redirect(w, r, "/account", 302)
+//			return
+//		}
+//		http.Redirect(w, r, "/signup", 302)
+//
+//	}
+//}
+//
+//func render(w http.ResponseWriter, name string, data interface{}) {
+//	tmpl, err := template.ParseGlob("templates/*.gohtml")
+//	if err != nil {
+//		http.Error(w, err.Error(), http.StatusInternalServerError)
+//	}
+//	tmpl.ExecuteTemplate(w, name, data)
+//}
+//
+//var cookieHandler = securecookie.New(
+//	securecookie.GenerateRandomKey(64),
+//	securecookie.GenerateRandomKey(32))
+//
+//func signin(w http.ResponseWriter, r *http.Request) {
+//	name := r.FormValue("uname")
+//	pass := r.FormValue("password")
+//	u := &User{Username: name, Password: pass}
+//	redirect := "/"
+//	if name != "" && pass != "" {
+//		if b, uuid := userExists(u); b == true {
+//			setSession(&User{Uuid: uuid}, w)
+//			redirect = "/account"
+//		} else {
+//			setMsg(w, "message", "please signup or enter a valid username and password!")
+//		}
+//	} else {
+//		setMsg(w, "message", "Username or Password field are empty!")
+//	}
+//	http.Redirect(w, r, redirect, 302)
+//}
+//
+//func inaccount(w http.ResponseWriter, r *http.Request) {
+//	uuid := getUuid(r)
+//	u := getUserFromUuid(uuid)
+//	if uuid != "" {
+//		render(w, "internal", u)
+//	} else {
+//		setMsg(w, "message", "Please login first!")
+//		http.Redirect(w, r, "/account", 302)
+//	}
+//}
+//
+//func logout(w http.ResponseWriter, r *http.Request) {
+//	clearSession(w, "session")
+//	http.Redirect(w, r, "/account", 302)
+//}
 
-//func login(w http.ResponseWriter, req *http.Request) {
-//
-//	pd := PageData{
-//		Title: "Login",
-//	}
-//
-//	err := tpl.ExecuteTemplate(w, "login.gohtml", pd)
-//	if err != nil {
-//		log.Println(err)
-//		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-//		return
-//	}
-//}
-//
-//func signup(w http.ResponseWriter, req *http.Request) {
-//
-//	pd := PageData{
-//		Title: "Sign Up",
-//	}
-//
-//	err := tpl.ExecuteTemplate(w, "signupold.gohtml", pd)
-//	if err != nil {
-//		log.Println(err)
-//		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-//		return
-//	}
-//}
+func account(w http.ResponseWriter, r *http.Request) {
+
+	pd := PageData{
+				Title: "Account",
+			}
+			err := tpl.ExecuteTemplate(w, "account.gohtml", pd)
+			if err != nil {
+				log.Println(err)
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+				return
+			}
+
+	//uuid := getUuid(r)
+	//if uuid != "" {
+	//	http.Redirect(w, r, "/inaccount", 302)
+	//	return
+	//}
+	//msg := getMsg(w, r, "message")
+	//var u = &User{}
+	//u.Errors = make(map[string]string)
+	//if msg != "" {
+	//	u.Errors["message"] = msg
+	//	render(w, "signin", u)
+	//} else {
+	//	u := &User{}
+	//	render(w, "signin", u)
+	//}
+
+}
